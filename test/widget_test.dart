2c2559ca1f87c01b5ca6a34728f7562313b077b7
jonier255 <jonier145@gmail.com>
2025-09-19 15:29:57 -0500

    cambios nombres de algunos files

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_a_c_soluciones/ui/main.dart';
+import 'package:flutter_a_c_soluciones/ui/login.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
